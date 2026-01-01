2bc5f27b3396f300c3c14abae0bd0e5cdb8da6fc
1nlbrn1 <86798663+1nlbrn1@users.noreply.github.com>
2026-01-02 02:21:26 +0500

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 
+
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
