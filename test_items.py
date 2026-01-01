4730f06bc4597db79059c19375b6ec7739683b93
1nlbrn1 <86798663+1nlbrn1@users.noreply.github.com>
2026-01-02 02:27:24 +0500

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 
 
-def test_guest_should_see_login_link(browser):
+def test_guest_should_see_basket_button(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     button = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
